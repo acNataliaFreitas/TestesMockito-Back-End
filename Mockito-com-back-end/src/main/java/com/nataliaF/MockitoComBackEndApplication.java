9784ff94b99e76834a4cd798ce9f528c70520641
Natalia Freitas <natyff1@hotmail.com>
2021-06-13 21:24:35 -0300

    Atividade de Testes juntamente com back-end sobre mockito

---
+++ Mockito-com-back-end/src/main/java/com/nataliaF/MockitoComBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.nataliaF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoComBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoComBackEndApplication.class, args);
+	}
+
+}
---
+++ Mockito-com-back-end/src/test/java/com/nataliaF/MockitoComBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nataliaF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoComBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
